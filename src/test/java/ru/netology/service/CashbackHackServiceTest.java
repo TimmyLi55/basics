0cc61b100573e4f85a5d3fccf09648ee445e85e0
TimmyLi <artelaw@gmail.com>
2022-10-29 12:41:50 +0600

    ветка с TESTNG и автотестами

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void CheckAmountLessThanTest() {
+
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CheckingMultipleValueTest() {
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CheckAmountMoreThan1000Test() {
+
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+}
